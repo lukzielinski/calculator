17a135f0d9ef3b4c622c227bc6c718ca8f69cbe0
ReQ <mrzielony22@gmail.com>
2023-08-26 12:40:09 +0200

    Added styles for main page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
-  <div id="nav">
+  <div class="main-elements">
+    cyce
   </div>
 </template>
 
 <style lang="scss">
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.main-elements {
+  width: 100vw;
+  height: 100vh;
+}
 </style>
