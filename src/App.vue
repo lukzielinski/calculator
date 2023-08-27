0c74633d27d5ab10c9cb67201274e376669e0515
ReQ <mrzielony22@gmail.com>
2023-08-27 11:24:57 +0200

    Added colors and shadows

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,10 @@ $border-radius: 20px;
 .main-elements {
   width: 100vw;
   height: 100vh;
-  background-color: white;
+  background-color: #0C0032;
 }
 .calculator > div {
-  border: 1px solid rgb(255, 72, 0);
+  border: 1px solid #190061;
 }
 .calculator{
   width: 100%;
@@ -41,5 +41,7 @@ $border-radius: 20px;
   display: grid;
   grid-template-rows: 35% 65%;
   border-radius: $border-radius;
+  background-color: #190061;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 }
 </style>
