5e3cb66fdaa3757c1d4ceff00f4280eac75d474e
ReQ <mrzielony22@gmail.com>
2023-08-27 10:37:39 +0200

    layout

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@
 </template>
 
 <style lang="scss">
+
+$border-radius: 20px;
+
 * {
   margin: 0;
   padding: 0;
@@ -20,14 +23,25 @@
 .main-elements {
   width: 100vw;
   height: 100vh;
+  
 }
 .calculator > div {
-  border: 1px solid red;
+  border: 1px solid rgb(255, 72, 0);
 }
 .calculator{
   width: 100%;
   height: 100%;
   display: grid;
   grid-template-rows: 35% 65%;
+  padding: 10%;
+  border: 1px solid red;
+}
+.output{
+  border-top-left-radius: $border-radius;
+  border-top-right-radius: $border-radius;
+}
+.buttons{
+  border-bottom-left-radius: $border-radius;
+  border-bottom-right-radius: $border-radius;
 }
 </style>
